33ff599b4b1079dd9833e86770ae65d27a19d013
Rohit Pawar <friends9850@gmail.com>
2023-06-11 22:11:06 +0530

    input from frontend

--- main.js
+++ main.js
@@ -1,6 +1,13 @@
 function AddRecord()
 {
-    let uri = `http://localhost:4000/addrecord?message=hiiiiiiiiiii`;
+    let inputRef = document.querySelector("#text1")
+    let message = inputRef.value;
+    let uri = `http://localhost:4000/addrecord?message=${message}`;
     //help to connect backend
     fetch(uri);
+
+    // for clearing the box
+    inputRef.value = "";
 }
+
+// connect mongodb with day11-backend
